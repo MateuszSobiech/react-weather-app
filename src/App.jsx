14380a5be8fc6ac7bd7faf6fb4f6dca5dcbf34f7
Mateusz Sobiech <lordpython8@gmail.com>
2021-12-09 20:44:35 +0100

    create hook: useWeather

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import styles from './App.module.scss';
+import { useWeather } from './hooks/useWeather';
 
 const App = () => {
-  const getBaseURL = (city) =>
-    `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=55b7bce44b262858d3201b409b5dcd65`;
-
   const [inputValue, setInputValue] = useState('Warsaw');
-  const [data, setData] = useState();
+  const { getWeatherData, data } = useWeather();
 
   const handleChange = (e) => {
     setInputValue(e.target.value);
@@ -15,33 +12,26 @@ const App = () => {
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      const url = getBaseURL(inputValue);
-      axios
-        .get(url)
-        .then((response) => setData(response.data))
-        .catch(() => setData());
+      getWeatherData(inputValue);
     }, 500);
 
     return () => clearTimeout(timer);
-  }, [inputValue]);
+  }, [inputValue, getWeatherData]);
 
   return (
     <div className={styles.App}>
       <header className={styles.AppHeader}>
         <input type="text" value={inputValue} onChange={handleChange} />
         <div className={styles.card}>
-          {data && (
+          {data.name ? (
             <div>
-              <p>Temperature: {Math.round((data.main.temp - 273.15) * 100) / 100} &#8451;</p>
-              <p>
-                Feels like: {Math.round((data.main.feels_like - 273.15) * 100) / 100} &#8451;
-              </p>
-              <img
-                src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
-                alt=""
-              />
-              <p className={styles.none}>{data.weather[0].main}</p>
+              <p>Temperature: {data.temp} &#8451;</p>
+              <p>Feels like: {data.feels} &#8451;</p>
+              <img src={data.iconSrc} alt="" />
+              <p className={styles.none}>{data.name}</p>
             </div>
+          ) : (
+            <p>Loading...</p>
           )}
         </div>
       </header>
---
+++ src/hooks/useWeather.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { useState } from 'react';
+
+export const useWeather = () => {
+  const getBaseURL = (city) =>
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=55b7bce44b262858d3201b409b5dcd65`;
+
+  const [data, setData] = useState();
+
+  const getWeatherData = (city) => {
+    const url = getBaseURL(city);
+    axios
+      .get(url)
+      .then((response) => setData(response.data))
+      .catch(() => setData());
+  };
+
+  return {
+    getWeatherData,
+    data: {
+      temp: getDegreesCelsius(data?.main.temp),
+      feels: getDegreesCelsius(data?.main.feels_like),
+      iconSrc: getIconSrc(data?.weather[0].icon),
+      name: data?.weather[0].main,
+    },
+  };
+};
+
+function getDegreesCelsius(number) {
+  return Math.round((number - 273.15) * 100) / 100;
+}
+
+function getIconSrc(icon) {
+  return `https://openweathermap.org/img/wn/${icon}@2x.png`;
+}
